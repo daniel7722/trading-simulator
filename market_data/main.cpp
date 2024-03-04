f205831dd324a662b91a2edd596c7db6066c7af2
Daniel Huang <74921405+daniel7722@users.noreply.github.com>
2024-03-04 14:48:03 +0000

    ci (DH): modified workflow file test workflow

--- market_data/main.cpp
+++ market_data/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! This is testing 10" << std::endl;
+    std::cout << "Hello, World! This is testing 11" << std::endl;
     return 0;
 }
