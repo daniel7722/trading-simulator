4654ab38a63f5c6bc2fee1e75fc76e272f8e8b74
Daniel Huang <74921405+daniel7722@users.noreply.github.com>
2024-03-10 14:48:28 +0000

    ci (DH): option_pricing testing if change made in option_pricing would trigger market_data's workflow

--- option_pricing/main.cpp
+++ option_pricing/main.cpp
@@ -8,7 +8,7 @@ int main() {
     double strikePrice = 110.0;
     double maturity = 1.0;
     int numPaths = 1000000;
-    int numThreads = 1000;
+    int numThreads = 2000;
 
     Pricing model(mu, sigma);
     double optionPrice = model.monteCarloOptionPrice(initialPrice, strikePrice, maturity, numPaths, numThreads);
