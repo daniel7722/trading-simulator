e9d3d5736e61081c82eb94df086d7b1eadc5bc59
Daniel Huang <74921405+daniel7722@users.noreply.github.com>
2024-03-10 14:56:35 +0000

    ci (DH): market_data test workflow separate command lines

--- market_data/main.cpp
+++ market_data/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! This is testing 16" << std::endl;
+    std::cout << "Hello, World! This is testing 17" << std::endl;
     return 0;
 }
