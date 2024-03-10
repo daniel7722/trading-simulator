632dc517869d3c7b1607b78e6c76cab87a79b3a3
Daniel Huang <74921405+daniel7722@users.noreply.github.com>
2024-03-10 14:58:11 +0000

    ci (DH): market_data set up correct directory for build

--- market_data/main.cpp
+++ market_data/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! This is testing 17" << std::endl;
+    std::cout << "Hello, World! This is testing 18" << std::endl;
     return 0;
 }
